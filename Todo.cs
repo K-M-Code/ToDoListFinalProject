793609fd14d2a573e00aa5e6aaf4fa94b3dd0202
AM <e2000598@edu.vamk.fi>
2024-04-18 20:12:40 +0300

    EF core install and db setup

---
+++ Todo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListFinalProject
+{
+    [Table("todos")]
+    public class Todo
+    {
+        [Key]
+        [Column("id")]
+        public Guid Id { get; set; }
+        [Required]
+        [Column("title")]
+        public string Title { get; set; }
+        [Required]
+        [Column("description")]
+        public string Description { get; set; }
+        [Required]
+        [Column("is_completed")]
+        public bool IsCompleted { get; set; }
+        [Required]
+        [Column("is_deleted")]
+        public bool IsDeleted { get; set; }
+        [Required]
+        [Column("is_important")]
+        public bool IsImportant { get; set; }
+    }
+}
